509f69eee734f1f157d9a155d6fe63f35b65f7a4
lllem <s.m.shemiakin@gmail.com>
2023-10-21 18:09:39 +0300

    Добавлена страница "О нас"

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,162 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <section class="bg-dark text-white py-5">
+    <div class="container max-w-screen-xl mx-auto px-4 py-5">
+
+      <div class="grid grid-cols-12 md:gap-6 lg:gap-20 justify-start justify-items-start content-start ">
+
+        <div class="col-span-12 md:col-span-12 lg:col-span-5 py-4 text-center mx-auto">
+          <img src="/assets/img/pages-illustrations/about/Asset 5.webp" class="w-full mx-auto" alt="">
+        </div>
+
+        <div class="col-span-12 md:col-span-12 lg:col-span-7">
+
+          <titleSpark h="2" white class="text-4xl mb-4">История компании</titleSpark>
+
+          <p class="mb-3 text-sm"><b class="font-bold">Lorem ipsum</b> dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.</p>
+
+          <p class="mb-3 text-sm">Lorem ipsum dolor sit amet, cons ectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.</p>
+
+          <p class="mb-3 text-sm">Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.</p>
+        </div>
+
+      </div>
+
+    </div>
+  </section>
+
+  <section class="py-5">
+    <div class="container max-w-screen-xl mx-auto px-4 py-5">
+
+      <titleSpark h="2" class="text-4xl mb-4">Коллектив</titleSpark>
+
+      <div class="grid grid-cols-12 md:gap-6 lg:gap-20 justify-start justify-items-start content-start ">
+        <div class="col-span-12 md:col-span-12 lg:col-span-7">
+
+          <p class="mb-3 text-sm"><b class="font-bold">Lorem ipsum</b> dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.</p>
+
+          <p class="mb-3 text-sm">Lorem ipsum dolor sit amet, cons ectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.</p>
+
+          <p class="mb-3 text-sm">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.</p>
+
+          <p class="mb-3 text-sm">Lorem ipsum dolor sit amet, cons ectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.</p>
+
+          <p class="mb-3 text-sm">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.</p>
+
+          <p class="mb-3 text-sm">Lorem ipsum dolor sit amet, cons ectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.</p>
+
+          <p class="mb-3 text-sm">Lorem ipsum dolor sit amet, cons ectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.</p>
+        </div>
+
+        <div class="col-span-12 md:col-span-12 lg:col-span-5">
+          <div class="map aspect-square bg-slate-200 grid grid-cols-3 grid-rows-3 gap-4 p-4 ">
+
+            <div class="aspect-square bg-sky-400 text-white font-bold col-start-2 row-start-1 col-end-3 text-center p-4">
+              <svg viewBox="0 0 19 13" fill="#fff" class="mb-2">
+                <text x="0" y="12">{{ teamQTY }}</text>
+              </svg>
+              <svg viewBox="0 0 75 16" fill="#fff">
+                <text x="0" y="13">ЧЕЛОВЕК</text>
+              </svg>
+            </div>
+
+            <div
+            v-for="item in team"
+            :key="item"
+            class="aspect-square bg-slate-400"
+            >
+              <img :src="`/assets/img/pages-illustrations/about/team/${item}`" class="cover">
+            </div>
+          </div>
+        </div>
+
+      </div>
+
+    </div>
+
+  </section>
+
+  <section class="news-block py-5 bg-slate-100 blue-border-top">
+    <div class="container max-w-screen-xl mx-auto px-4 py-9">
+
+      <div class="grid grid-cols-12 md:gap-x-12 gap-y-8 border-bottom py-5">
+
+        <div class="col-span-12 md:col-span-12 lg:col-span-5">
+          <div
+          v-for="filial in filials"
+          :key="filial.title"
+          class="mb-4"
+          >
+            <h3 class="text-sky-600">{{ filial.title }}</h3>
+            <p class="text-sm">{{ filial.description }}</p>
+          </div>
+        </div>
+
+        <div class="col-span-12 md:col-span-12 lg:col-span-7">
+          <div class="map aspect-square md:aspect-video lg:aspect-video xl:aspect-[21/9] bg-slate-300 w-full block">Карта</div>
+        </div>
+
+      </div>
+    </div>
+  </section>
 </template>
+
+<script>
+export default {
+  components: {},
+  data() {
+    return {
+      teamQTY: 90,
+      team: [
+        'team1.webp',
+        'team2.webp',
+        'team3.webp',
+        'team4.webp',
+        'team5.webp',
+        'team6.webp',
+        'team7.webp',
+        'team8.webp',
+      ],
+      filials: [
+        {
+          title: 'Филиал №1',
+          description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.',
+        },
+        {
+          title: 'Филиал №2',
+          description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.',
+        },
+        {
+          title: 'Филиал №3',
+          description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.',
+        },
+        {
+          title: 'Филиал №4',
+          description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.',
+        },
+      ],
+    }
+  },
+}
+</script>
+
+<style lang="scss">
+.logo_illustration {
+  position: relative;
+
+  .logo_illustration__logo {
+    position: absolute;
+    top: 18%;
+    bottom: 18%;
+    height: 64%;
+    width: 100%;
+  }
+  .logo_illustration__lines {}
+}
+
+.grid {
+  justify-content: stretch;
+  justify-items: stretch;
+  align-content: stretch;
+  align-items: stretch;
+}
+</style>
