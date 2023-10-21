ff7678fdf85da56d6e8896b7dd8bc26622dceeac
lllem <s.m.shemiakin@gmail.com>
2023-10-21 20:31:22 +0300

    Добавлена страница "Сертификаты"

--- src/views/CertificatesView.vue
+++ src/views/CertificatesView.vue
@@ -1,99 +1,63 @@
 <template>
   <section class="py-5">
-    <div class="container max-w-screen-xl mx-auto px-4 py-5">
+    <div class="container max-w-screen-xl mx-auto px-4 py-9">
 
-      <div class="grid grid-cols-12 md:gap-6 lg:gap-20 justify-start justify-items-start content-start ">
+      <titleSpark h="2" class="text-4xl mb-4 uppercase">Документация</titleSpark>
 
-        <div class="col-span-12 md:col-span-12 lg:col-span-7">
+      <p class="mb-3 text-sm">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.</p>
 
-          <titleSpark h="2" class="text-4xl mb-4">Поставка РЭК</titleSpark>
+      <p class="mb-3 text-sm">Lorem ipsum dolor sit amet, cons ectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.</p>
 
-          <p class="mb-3 text-sm">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.</p>
-
-          <p class="mb-3 text-sm">Lorem ipsum dolor sit amet, cons ectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.</p>
-
-          <p class="mb-3 text-sm">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.</p>
-
-          <p class="mb-3 text-sm">Lorem ipsum dolor sit amet, cons ectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.</p>
-
-          <p class="mb-3 text-sm">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.</p>
-
-          <p class="mb-3 text-sm">Lorem ipsum dolor sit amet, cons ectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.</p>
-
-          <p class="mb-3 text-sm">Lorem ipsum dolor sit amet, cons ectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.</p>
-
-          <div class="mb-3">
-            <a href="#" class="text-sky-600 hover:text-sky-600 underline mb-1 whitespace-nowrap"><ClipboardDocumentListIcon class="icon"/> <span>СКАЧАТЬ КАТАЛОГ РЭК</span></a>
-          </div>
-        </div>
-
-        <div class="col-span-12 md:col-span-12 lg:col-span-5 py-4">
-          <img src="/assets/img/pages-illustrations/rec-supplies/Asset 2.webp" class="w-full" alt="ПАРИТЕТ">
-        </div>
-
-      </div>
-
-      <div class="my-10 text-center">
-        <pBtn href="#" class="mx-auto">Перейти в магазин</pBtn>
-      </div>
-
+      <p class="mb-3 text-sm">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.</p>
     </div>
   </section>
 
-  <section class="bg-dark text-white py-5">
+  <section class="pt-5">
     <div class="container max-w-screen-xl mx-auto px-4 py-5">
+      <titleSpark h="2" class="text-4xl mb-4 uppercase">Сертификаты</titleSpark>
+    </div>
 
-      <div class="grid grid-cols-12 md:gap-6 lg:gap-20 justify-start justify-items-start content-start ">
+    <div class="bg-dark text-white py-9 blue-border-top-xl">
+      <div class="container max-w-screen-xl mx-auto px-4 py-5">
+        <div class="grid grid-cols-3 md:gap-6 lg:gap-20 justify-start justify-items-start content-start ">
 
-        <div class="col-span-12 md:col-span-12 lg:col-span-7">
+          <a href="/files/certificates/ИСО 9001.pdf" class="my-4">
+            <img src="/assets/img/pages-illustrations/certificates/ИСО-9001.webp" class="w-full" alt="ИСО-9001">
+          </a>
 
-          <titleSpark h="2" white class="text-4xl mb-4">РЭК</titleSpark>
+          <a href="/files/certificates/ИСО 9001 приложение.pdf" class="my-4">
+            <img src="/assets/img/pages-illustrations/certificates/ИСО-9001-приложение.webp" class="w-full" alt="ИСО-9001-приложение">
+          </a>
 
-          <p class="mb-3 text-sm">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.</p>
+          <a href="/files/certificates/ИСО 9001 Разрешение.pdf" class="my-4">
+            <img src="/assets/img/pages-illustrations/certificates/ИСО-9001-Разрешение.webp" class="w-full" alt="">
+          </a>
 
-          <p class="mb-3 text-sm">Lorem ipsum dolor sit amet, cons ectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.</p>
-
-          <p class="mb-3 text-sm">Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.</p>
         </div>
-
-        <div class="col-span-12 md:col-span-12 lg:col-span-5 py-4">
-          <img src="/assets/img/pages-illustrations/rec-supplies/rec-supplies.webp" class="w-full" alt="">
-        </div>
-
       </div>
-
     </div>
   </section>
 
-  <section class="bg-dark text-white py-5">
-    <div class="container max-w-screen-xl mx-auto px-4 grid lg:grid-cols-2 gap-x-6 py-5 items-center">
+  <section class="py-9">
+    <div class="container max-w-screen-xl mx-auto px-4 py-9">
 
-      <div class="opacity-20">
-        <img class="cover" src="/assets/img/pages-illustrations/rec-supplies/scheme.webp" alt="">
-      </div>
+      <titleSpark h="2" class="text-4xl mb-4 uppercase">Наши партнёры</titleSpark>
 
-      <div class="py-4">
-        <titleSpark h="1" white class="text-2xl mb-4 uppercase">Почему мы?</titleSpark>
-
-        <p class="mb-3 text-sm"><span class="text-sky-400 font-bold">АССОРТИМЕНТ</span> dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.</p>
-
-        <p class="mb-3 text-sm"><span class="text-sky-400 font-bold">ЦЕНЫ</span> dolor sit amet, cons ectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.</p>
-
-        <p class="mb-3 text-sm"><span class="text-sky-400 font-bold">ГАРАНТИЯ И ДОСТАВКА</span> enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.</p>
+      <div class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-4 mt-12">
+        <div
+        v-for="logo in 12"
+        :key="logo"
+        class="bg-slate-300 aspect-video p-5 text-center"
+        >Логотип</div>
       </div>
 
     </div>
-
   </section>
 </template>
 
 <script>
-import {ClipboardDocumentListIcon} from '@heroicons/vue/24/solid'
-
 export default {
-  components: {
-    ClipboardDocumentListIcon,
-  },
+  components: {},
 }
 </script>
 
